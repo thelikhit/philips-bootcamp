84d5faa0f731d01488c8f657f2129f27b173c2d2
Likhit Jain <48644772+thelikhit@users.noreply.github.com>
2021-09-29 10:42:21 +0530

    Create Vehicle.cs

---
+++ Vehicle.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Vehicle {
+}
+
+class Car: Vehicle {
+	Engine engine = new Engine();
+	Transmission transmission = new Transmission();
+}
+
+class Engine {
+}
+
+class Transmission {
+}
+				
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+	}
+}
